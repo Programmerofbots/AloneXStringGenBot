f0d5aa845af62dbb227e0a9bf73d21640094e3aa
HACKER OP <149420286+Programmerofbots@users.noreply.github.com>
2024-01-31 17:39:39 +0530

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@AloneXBots"))
+            await client(JoinChannelRequest("@Princes_log_support_group"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("AloneXBots")
+            await client.join_chat("@Princes_log_support_group")
     except KeyError:
         pass
     try:
